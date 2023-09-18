85941b5821795e2b7a6d4902b21ea008cee1de6e
abdohosny1 <make.y90@yahoo.com>
2023-09-18 20:48:43 +0300

    Add project files.

---
+++ UseNpm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UseNpm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ UseNpm/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿// Configure your AWS credentials and region
+AWS.config.credentials = new AWS.Credentials({
+    accessKeyId: "YourAccessKey",
+    secretAccessKey: "YourSecretKey",
+});
+
+AWS.config.region = "us-east-1"; // Replace with your desired AWS region
+
+// Create an S3 client
+const s3 = new AWS.S3();
+
+// Use the S3 client to interact with Amazon S3 (e.g., list buckets, upload/download objects, etc.)
+const params = {
+    Bucket: "your-bucket-name",
+};
+
+s3.listBuckets(params, (err, data) => {
+    if (err) {
+        console.error("Error listing buckets:", err);
+    } else {
+        console.log("Buckets:", data.Buckets);
+    }
+});
